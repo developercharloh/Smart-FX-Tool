728f3b70247b229d6aa4d25b8cedd61dfff5a202
developercharloh <ckyalo010@gmail.com>
2026-07-28 17:05:13 +0000

    Auto-push: 2026-07-28 17:05:12

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,23 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttp, { type Options as PinoHttpOptions } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+const httpLogger = (pinoHttp as unknown as (opts: PinoHttpOptions) => any)({
+  logger,
+  serializers: {
+    req(req: any) {
+      return { id: req.id, method: req.method, url: req.url?.split("?")[0] };
     },
-  }),
-);
+    res(res: any) {
+      return { statusCode: res.statusCode };
+    },
+  },
+});
+app.use(httpLogger);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
